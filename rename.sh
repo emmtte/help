cfb169693af2db97fab99886db5e64c49b47ed5b
Emmanuel CHARETTE <emmanuel.charette@gmail.com>
2014-09-08 14:53:02 +0200

    Update rename.sh

--- rename.sh
+++ rename.sh
@@ -5,13 +5,13 @@
 
 # Loop through the list, renaming each to a date/time based name
 COUNTER=1
-find $1 -iname "*.jpg" -type f | sort | while read FILE ; do 
+find "$1" -iname "*.jpg" -type f | sort | while read FILE ; do 
         EXIFDATE=`exif $FILE | grep "Date" | grep -v "(" | cut -d '|' -f 2 | tr ' ' '_'`
         #EXIFDATE=`exif $FILE | grep "Date" | grep -v "(" | cut -d '|' -f 2 | cut -d ' ' -f 1 | tr ':' '-'`
 	#EXIFTIME=`exif $FILE | grep "Date" | grep -v "(" | cut -d '|' -f 2 | cut -d ' ' -f 2 | tr ':' '.'`
 	#NAME="$EXIFDATE $EXIFTIME $COUNTER"
-	NEWFILE = "$EXIFDATE_$COUNTER"
-	mv -u $FILE $NEWFILE.JPG
+	NEWFILE = "$EXIFDATE_$COUNTER.JPG"
+	mv -u "$FILE" "$NEWFILE"
 	COUNTER=$((COUNTER + 1))
 done
 
@@ -19,9 +19,9 @@ done
 # Set up a counter for the file names
 COUNTER=1
 # Loop through list2, renaming as "pic###.jpg"
-find $1 -iname "*.jpg" -type f | sort | while read FILE ; do 
+find "$1" -iname "*.jpg" -type f | sort | while read FILE ; do 
         NEWNAME="IMG_$(printf "%05d" $COUNTER).JPG"
-        mv $FILE $NEWNAME
+        mv "$FILE" "$NEWNAME"
         # Increment COUNTER
         COUNTER=$((COUNTER + 1))
 done
