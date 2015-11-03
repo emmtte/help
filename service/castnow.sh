a6ed1415c9c73f328391da8dd7a54c34a21f8ae8
Emmanuel CHARETTE <emmanuel.charette@gmail.com>
2015-11-03 15:37:27 +0100

    Update castnow.sh

--- service/castnow.sh
+++ service/castnow.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 clear
 nodejs /usr/local/bin/castnow "http://hi1.streamingsoundtracks.com:8000" --type "audio/wav"
 nodejs /usr/local/bin/castnow "http://hi5.streamingsoundtracks.com" --type "audio/wav"
